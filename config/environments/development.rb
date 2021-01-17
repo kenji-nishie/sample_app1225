37fc399bcdce097c4afa74f1e11523e25c400945
kenji-nishie <kenji.nishie@gmail.com>
2021-01-17 22:13:10 +0900

    add name to hostname

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   host = 'localhost:3000'                     # ローカル環境
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
